2f22ed232e871219bf6fdbfa855e2f3624814a61
V1ctorW1ll1an <victorwillian1012@gmail.com>
2021-05-16 12:45:57 -0300

    feat(all): react project setup
    This project aims to create a setup with bootstrap, sass and typescript for creating websites with
    react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Button } from "react-bootstrap"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1
+                    style={{
+                        color: "#61DAFB",
+                    }}
+                >
+                    Setup!
+                </h1>
+                <Button
+                    onClick={() => alert("Hello World!!!")}
+                    variant="primary"
+                    size="lg"
+                    active
+                >
+                    Click me
+                </Button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/modules/README.md
@@ -0,0 +1,3 @@
+## Description
+
+### Put here all the modules of your project, usually the modules contain a page or view made up of several layouts that in general are made up of components
